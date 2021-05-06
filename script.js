15d783e9f726f329ed50661cc61c23656c203da7
MaxSoh <max.sohbh@gmail.com>
2021-05-06 09:02:00 +0800

    book object constructor

---
+++ script.js
@@ -0,0 +1,39 @@
+//book class / object constructor
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+//method display book, remove book
+function displayBook() {
+    const myLibrary = [
+        {
+            title: "Lord of the Rings",
+            author: 'Tolkien',
+            pages: 600,
+            read: false
+        },
+        {
+            title: "Harry Potter",
+            author: "JK Rowling",
+            pages: 900,
+            read: true
+        }
+    ];
+}
+
+//loop book array and call the method to add books to list
+
+//add books to table row
+
+// handle storage
+
+//event listen to display books: document laod
+
+// event to listen to clicks
+
+
+//event to remove a boook with delete button
+
