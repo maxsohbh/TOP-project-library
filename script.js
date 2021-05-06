c57d86ee66aedd466295a3aeec50b406880becef
MaxSoh <max.sohbh@gmail.com>
2021-05-06 10:15:43 +0800

    delete book function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//book class / object constructor
+// object constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -27,6 +27,7 @@ function displayBook() {
     books.forEach((book) => listIn(book));
 }
 
+//add books to table row
 function listIn(book) {
     const list = document.querySelector("#book-list");
     const row = document.createElement("tr");
@@ -34,23 +35,48 @@ function listIn(book) {
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pages}</td>
-    <td><a href="#" class='btn btn-info'></td>
-    <td><a href="#" class='btn btn-danger delete'></td>
+    <td><a href="#" class='btn togg'>${book.read ? `Read` : `Not Read`}</a></td>
+    <td><a href="#" class='btn btn-danger delete'>DEL</td>
     `
     list.appendChild(row);
 }
 
-
-//add books to table row
-
 // handle storage
 
+
+
+
 //event listen to display books: document laod
-
+document.addEventListener("DOMContentLoaded", displayBook)
 // event to listen to clicks
+document.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").value;
 
+    const newBook = new Book(title, author, pages, read);
+    // console.log(newBook);
+    //add book to list
+    listIn(newBook);
 
-//event to remove a boook with delete button
+    //clearfields
+    document.querySelector("#title").value = '';
+    document.querySelector("#author").value = '';
+    document.querySelector("#pages").value = '';
+    document.querySelector("#read").value = '';
+})
 
+//event to remove a book with delete button
+document.querySelector("#book-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.parentElement.remove();
+    }
 
-displayBook();
+    //work in progress, how to change the object read status to false
+    if (e.target.classList.contains("togg")) {
+        // e.target.innerHTML = `boop`;
+        console.log(e.target);
+    }
+})
